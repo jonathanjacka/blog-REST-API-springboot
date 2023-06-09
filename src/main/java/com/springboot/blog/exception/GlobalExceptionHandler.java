cfd322d999a7326c2893cf35add6bf011d5d7dd1
Jonathan Jacka <jonathanjacka@gmail.com>
2023-06-09 08:49:26 -0500

    Adds global exception handler

--- src/main/java/com/springboot/blog/exception/GlobalExceptionHandler.java
+++ src/main/java/com/springboot/blog/exception/GlobalExceptionHandler.java
@@ -26,4 +26,10 @@ public class GlobalExceptionHandler {
     }
 
     //handle global exceptions
+    @ExceptionHandler(Exception.class) //Since all exceptions extend (directly/indirectly) Exception class, therefore global scope
+    public ResponseEntity<ErrorDetails> handleGlobalException(Exception exception, WebRequest webRequest){
+        ErrorDetails errorDetails = new ErrorDetails(new Date(), exception.getMessage(), webRequest.getDescription(false));
+        return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
 }
