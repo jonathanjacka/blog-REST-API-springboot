1e1bf556128d3dab03ac0b0dd9152346746f99c8
Jonathan Jacka <jonathanjacka@gmail.com>
2023-06-23 20:12:49 -0500

    Adds Role and User models

---
+++ src/main/java/com/springboot/blog/model/User.java
@@ -0,0 +1,42 @@
+package com.springboot.blog.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Set;
+
+/*
+Lombok's dependency handles getters and setters as well as constructors
+Normally, you @Data for all getter. setter, toString methods.
+However, Model Mapper conflicts with Lombok by using own overwriting of toString - returns comments set as empty
+Therefore use only @Getter and @Setter annotations to return comments set correctly
+*/
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    @Column(nullable = false, unique = true)
+    private String username;
+    @Column(nullable = false, unique = true)
+    private String email;
+    @Column(nullable = false)
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id")
+    )
+    private Set<Role> roles;
+}
