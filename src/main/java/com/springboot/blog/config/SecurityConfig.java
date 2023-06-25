bbccc8df577f5c9247bd6325c6d44a2484e6835f
Jonathan Jacka <jonathanjacka@gmail.com>
2023-06-25 05:55:33 -0500

    Adds userDetailsService injection to SecurityConfig

--- src/main/java/com/springboot/blog/config/SecurityConfig.java
+++ src/main/java/com/springboot/blog/config/SecurityConfig.java
@@ -3,7 +3,9 @@ package com.springboot.blog.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -18,10 +20,24 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableMethodSecurity //needed for roles configuration - allows adding @Preauthorize to controllers
 public class SecurityConfig {
 
+    private UserDetailsService userDetailsService;
+
+    //Constructor-based dependency conversion
+    public SecurityConfig(UserDetailsService userDetailsService){
+        this.userDetailsService = userDetailsService;
+    }
+
     @Bean
     public static PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
+
+    //Automatically does auth with userDetailsService and PasswordEncoder, don't need to explicitly specify
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         http.csrf(csrf -> csrf.disable()).authorizeHttpRequests((auth) ->
@@ -31,6 +47,11 @@ public class SecurityConfig {
         return http.build();
     }
 
+    /*
+
+    In-memory objects no longer required
+    Imports remain but are unused too
+
     @Bean
     public UserDetailsService userDetailsService(){
         UserDetails user = User.builder().username("user").password(passwordEncoder().encode("user")).roles("USER").build();
@@ -39,4 +60,5 @@ public class SecurityConfig {
 
         return new InMemoryUserDetailsManager(user, jonathan, admin);
     }
+    */
 }
