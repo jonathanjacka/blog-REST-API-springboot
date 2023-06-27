cd927a9b6d004a969de606c3fbe2b7fc5c288e3c
Jonathan Jacka <jonathanjacka@gmail.com>
2023-06-26 20:16:56 -0500

    Adds register service and controller

--- src/main/java/com/springboot/blog/controller/AuthController.java
+++ src/main/java/com/springboot/blog/controller/AuthController.java
@@ -1,7 +1,9 @@
 package com.springboot.blog.controller;
 
 import com.springboot.blog.payload.LoginDto;
+import com.springboot.blog.payload.RegisterDto;
 import com.springboot.blog.service.AuthService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,5 +22,12 @@ public class AuthController {
     public ResponseEntity<String> login(@RequestBody LoginDto loginDto) {
         String response = authService.login(loginDto);
         return ResponseEntity.ok(response);
+    }
+
+    //Register REST API
+    @PostMapping(value = {"/register", "/signup"})
+    public ResponseEntity<String> register(@RequestBody RegisterDto registerDto) {
+        String response = authService.register(registerDto);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
 }
