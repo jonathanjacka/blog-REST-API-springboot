a5b435d4a1ea93b4f14d483f948398c655527b86
Jonathan Jacka <jonathanjacka@gmail.com>
2023-07-15 06:36:32 -0500

    Updates auth controller with annotations for docs

--- src/main/java/com/springboot/blog/controller/AuthController.java
+++ src/main/java/com/springboot/blog/controller/AuthController.java
@@ -4,6 +4,9 @@ import com.springboot.blog.payload.JwtAuthResponse;
 import com.springboot.blog.payload.LoginDto;
 import com.springboot.blog.payload.RegisterDto;
 import com.springboot.blog.service.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -23,7 +26,15 @@ public class AuthController {
     }
 
     //Log in/Sign in REST API
-    @PostMapping(value = {"/login", "/signin"})
+    @Operation(
+            summary = "User Login REST API",
+            description = "Allows user to login.  Returns a valid token on success.  Requires entry of valid username or email, and password."
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Http Status 200 SUCCESS"
+    )
+    @PostMapping(value = {"/login"})
     public ResponseEntity<JwtAuthResponse> login(@RequestBody LoginDto loginDto) {
         String token = authService.login(loginDto);
         JwtAuthResponse jwtAuthResponse = new JwtAuthResponse();
@@ -32,7 +43,15 @@ public class AuthController {
     }
 
     //Register REST API
-    @PostMapping(value = {"/register", "/signup"})
+    @Operation(
+            summary = "User Register REST API",
+            description = "Registers user.  Returns a String message as confirmation on success.  Requires entry of valid name, username, email, and password."
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Http Status 200 SUCCESS"
+    )
+    @PostMapping(value = {"/register"})
     public ResponseEntity<String> register(@RequestBody RegisterDto registerDto) {
         String response = authService.register(registerDto);
         return new ResponseEntity<>(response, HttpStatus.CREATED);
