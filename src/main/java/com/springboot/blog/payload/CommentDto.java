26a68f2a05d873276733d2cceac43aa47d83b2b9
Jonathan Jacka <jonathanjacka@gmail.com>
2023-07-15 07:05:45 -0500

    Updates comment dto with annotations for docs

--- src/main/java/com/springboot/blog/payload/CommentDto.java
+++ src/main/java/com/springboot/blog/payload/CommentDto.java
@@ -1,22 +1,39 @@
 package com.springboot.blog.payload;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
+//Swagger Docs
+@Schema(
+        description = "Comment Data Transfer Object Model Information"
+)
 public class CommentDto {
     private long id;
 
     @NotEmpty(message = "Name should not be empty")
+    //Swagger Docs
+    @Schema(
+            description = "Name of commentator"
+    )
     private String name;
 
     @NotEmpty(message = "Email should not be empty")
     @Email(message = "Email must be in valid format")
+    //Swagger Docs
+    @Schema(
+            description = "Email address of commentator"
+    )
     private String email;
 
     @NotEmpty(message = "Body of comment should not be empty")
     @Size(min = 10, message = "Content length must be 10 characters of greater")
+    //Swagger Docs
+    @Schema(
+            description = "Comment content/body"
+    )
     private String body;
 }
