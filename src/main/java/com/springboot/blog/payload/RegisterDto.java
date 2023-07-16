4c615bcd81a4b753fa03ef491a701820db3a212b
Jonathan Jacka <jonathanjacka@gmail.com>
2023-07-16 05:40:58 -0500

    Updates register dto with annotations for docs

--- src/main/java/com/springboot/blog/payload/RegisterDto.java
+++ src/main/java/com/springboot/blog/payload/RegisterDto.java
@@ -1,5 +1,6 @@
 package com.springboot.blog.payload;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,9 +10,29 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+//Swagger Docs
+@Schema(
+        description = "Register Data Transfer Object Model Information - returned on successful login"
+)
 public class RegisterDto {
+    //Swagger Docs
+    @Schema(
+            description = "Name of registrant"
+    )
     private String name;
+    //Swagger Docs
+    @Schema(
+            description = "Username of registrant - must be unique"
+    )
     private String username;
+    //Swagger Docs
+    @Schema(
+            description = "Email of registrant - must be unique"
+    )
     private String email;
+    //Swagger Docs
+    @Schema(
+            description = "Password of registrant"
+    )
     private String password;
 }
